2b4bac410ebd5d0f44760d32a9f2f43254c8303a
Abel <86670708+Abel5173@users.noreply.github.com>
2023-08-19 18:48:12 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,8 +6,7 @@ starts a Flask web application
 from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
